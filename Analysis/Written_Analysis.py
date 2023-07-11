9d9d6f4b599952a4f12761d20739ee067965c05c
Ashley Ley <yakopeca@gmail.com>
2023-07-11 13:48:17 -0500

    Update Written Analysis

--- Analysis/Written_Analysis.py
+++ Analysis/Written_Analysis.py
@@ -1,7 +1,7 @@
 # Results and Conclusions
 
 #     This project involves analysis of district-wide standardized test results using Pandas DataFrames. The goal is to provide insights and identify trends in 
-#     school performance to help the school board and mayor make strategic decisions regarding future schcool budgets and priorities.
+#     school performance to help the school board and mayor make strategic decisions regarding future school budgets and priorities.
 
 # The analysis of the school district data provides several insights into school performance and trends. 
 # Here are two notable observations:
@@ -9,7 +9,7 @@
     # 1. School Type and Performance
 
         # Charter schools are consistently outperforming district schools in terms of average math and reading scores. This is consistent for the passing rates as well. 
-        # The top performing schoosl are all charter schools, while the bottom performing schools are all district schools.
+        # The top performing schools are all charter schools, while the bottom performing schools are all district schools.
         # It was also noted that district schools have a larger student population than the charter schools. 
     
     # 2. School Spending and Performance 
